7b89302bd57a52070a1b026c3b49bba736e8f82e
Chamith Lakshan <70762572+ClaZieR@users.noreply.github.com>
2023-04-21 22:17:24 +0530

    sorting fixed

--- frontend/src/components/ProductManagement/Product.js
+++ frontend/src/components/ProductManagement/Product.js
@@ -1,102 +1,118 @@
-import React from 'react';
-import FirstImage from "../../img/img1.png";
-import SecondImage from "../../img/img2.png";
-import ThirdImage from "../../img/img3.png";
-import FourthImage from "../../img/img4.png";
-import "../../css/product.css";
+import React, { useEffect, useState } from 'react';
+import '../../css/product.css';
 
 export default function Product() {
-    return (
-        <div className="container">
-            <div className="col-md-4">
-            </div>
-            <div className="row justify-content-end">
-                <div className="col-md-8">
+  const [products, setProducts] = useState([]);
+  const [priceRange, setPriceRange] = useState("All Price");
+  const [category, setCategory] = useState("All");
 
-                    <h2 className="text-left product-main-header">Product List</h2>
-                    <h6 className="product-view-name">Viewing 20 of 500 products</h6>
-                    <h6 className="sort-product">Sort by Newest Items</h6>
-                    <div className='row'>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={FirstImage} alt='FirstImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={ThirdImage} alt='ThirdImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={FourthImage} alt='FourthImage mt-4' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={FirstImage} alt='FirstImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={FirstImage} alt='FirstImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                        <div className='col-md-3 image-container mt-4'>
-                            <img src={SecondImage} alt='SecondImage' className='product-image-container' />
-                            <h6 className='product-header'>T-Shirt</h6>
-                            <h6 className='product-price'>RS.3000.00</h6>
-                            <button className='text-white product-btn'>Add to Cart</button>
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:5000/products/getProducts');
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  const handlePriceRangeChange = (range) => {
+    setPriceRange(range);
+  };
+
+  const handleCategoryChange = (category) => {
+    setCategory(category);
+    };
+
+  const filterByPriceRange = () => {
+    if (priceRange === 'All Price') {
+      return products;
+    } else if (priceRange === '1000-3000') {
+      return products.filter((product) => product.price >= 1000 && product.price <= 3000);
+    } else if (priceRange === '3000-5000') {
+      return products.filter((product) => product.price >= 3000 && product.price <= 5000);
+    } else if (priceRange === '5000-7000') {
+        return products.filter((product) => product.price >= 5000 && product.price <= 7000);
+    }else if (priceRange === '7000-10000') {
+        return products.filter((product) => product.price >= 7000 && product.price <= 10000);
+    }
+};
+
+    const filterByCategory = () => {
+        if (category === 'All') {
+            return products;
+        } else if (category === 'Women') {
+            return products.filter((product)=>product.category === "Women's");
+        } else if (category === 'Men') {
+              return products.filter((product) => product.category === "Men's");
+        }else if (category === 'Kids') {
+              return products.filter((product) => product.category === "Kids");
+              
+        }
+    };
+    
+    const filteredProducts = () => filterByCategory().filter((product) => filterByPriceRange().includes(product));
+
+
+  return (
+    <div className="container">
+        <div className="col-md-4">
+            <h6 className="sort-product">Sort by Price Range</h6>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="priceRange" id="allPrice" value="All Price" onChange={() => handlePriceRangeChange('All Price')} checked={priceRange === 'All Price'} />
+                <label className="form-check-label" htmlFor="allPrice">All Price</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="priceRange" id="1000-3000" value="1000-3000" onChange={() => handlePriceRangeChange('1000-3000')} checked={priceRange === '1000-3000'} />
+                <label className="form-check-label" htmlFor="1000-3000">$1000 - $3000</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="priceRange" id="3000-5000" value="3000-5000" onChange={() => handlePriceRangeChange('3000-5000')} checked={priceRange === '3000-5000'} />
+                <label className="form-check-label" htmlFor="3000-5000">$3000-5000</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="priceRange" id="1000-3000" value="1000-3000" onChange={() => handlePriceRangeChange('5000-7000')} checked={priceRange === '5000-7000'} />
+                <label className="form-check-label" htmlFor="5000-7000">$5000 - $7000</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="priceRange" id="1000-3000" value="1000-3000" onChange={() => handlePriceRangeChange('7000-10000')} checked={priceRange === '7000-10000'} />
+                <label className="form-check-label" htmlFor="7000-10000">$7000 - $10000</label>
+            </div>
+            <h6 className="sort-product">Sort by Category</h6>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="category" id="all" value="All" onChange={() => handleCategoryChange('All')} checked={category === 'All'} />
+                <label className="form-check-label" htmlFor="all">All</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="category" id="women" value="Women" onChange={() => handleCategoryChange('Women')} />
+                <label className="form-check-label" htmlFor="all">Women</label>
+            </div>
+            <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="category" id="men" value="Men" onChange={() => handleCategoryChange('Men')} />
+                <label className="form-check-label" htmlFor="all">Men</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="category" id="kids" value="kids" onChange={() => handleCategoryChange('Kids')}  /> 
+                <label className="form-check-label" htmlFor="all">Kids</label>
             </div>
         </div>
-
-    )
+      <div className="row justify-content-end">
+        <div className="col-md-8">
+          <h2 className="text-left product-main-header">Product List</h2>
+          <h6 className="product-view-name">Viewing {filteredProducts().length} of {products.length} products</h6>
+            <div className='row'>
+            {filteredProducts().map(product => (
+              <div className='col-md-3 image-container mt-4' key={product.id}>
+                <img src={product.image} alt={product.name} className='product-image-container' />
+                <h6 className='product-header'>{product.name}</h6>
+                <h7 className='product-description'>{product.description}</h7>
+                <h6 className='product-price'>{product.price}</h6>
+                <button className='text-white product-btn'>Add to Cart</button>
+              </div>
+            ))}
+            
+            </div>
+            </div>
+            </div>
+</div>
+)
 }
